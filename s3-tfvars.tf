516671db8ad4e18437ca2a9337569ae097a80254
Chris Wright <chris@dxw.com>
2023-07-19 12:28:02 +0100

    Add terraform-aws-tfvars-s3 module
    * This module will be used to store the terraform variables within a
      secure S3 bucket, so that they can be kept up to date in a centralised
      location to be shared with the project team.
    * The module currently has no releases, so `main` is currently being
      used.

---
+++ s3-tfvars.tf
@@ -0,0 +1,9 @@
+module "aws_tfvars_s3" {
+  # This module currently doesn't have a Release
+  # tflint-ignore: terraform_module_pinned_source
+  source = "github.com/dxw/terraform-aws-tfvars-s3?ref=main"
+
+  project_name             = local.project_name
+  enable_s3_bucket_logging = local.tfvars_s3_enable_s3_bucket_logging
+  logging_bucket_retention = local.tfvars_s3_logging_bucket_retention
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" {
+  description = "Project name to be used as a prefix for all resources"
+  type        = string
+}
+
+variable "tfvars_s3_enable_s3_bucket_logging" {
+  description = "Enable S3 bucket logging on the tfvars S3 bucket"
+  type        = bool
+}
+
+variable "tfvars_s3_logging_bucket_retention" {
+  description = "tfvars S3 Logging bucket retention in days. Set to 0 to keep all logs."
+  type        = number
+}
